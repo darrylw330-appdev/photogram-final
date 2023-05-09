568d4b1712259227b2ecda937af3adf0bea63424
Darryl Williams <williamsdarryl330@gmail.com>
2023-05-09 14:08:03 +0000

    updated comments_controller still 98%

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,10 +19,10 @@ class CommentsController < ApplicationController
 
   def create
     the_comment = Comment.new
-    the_comment.author_id = params.fetch("query_author_id")
+    the_comment.author_id = current_user.id
     the_comment.body = params.fetch("query_body")
-    the_comment.photo_id = params.fetch("query_photo_id")
-  
+    the_comment.photo_id = params.fetch(:photo_id)
+    
     if the_comment.valid?
       the_comment.save
       redirect_to("/photos/#{the_comment.photo_id}", { :notice => "Comment created successfully." })
@@ -30,6 +30,7 @@ class CommentsController < ApplicationController
       redirect_to("/photos/#{the_comment.photo_id}", { :alert => the_comment.errors.full_messages.to_sentence })
     end
   end
+  
 
   def update
     the_id = params.fetch("path_id")
